b1b36a1387a449bb0b067d39117675c1e1bd5e8f
Gopala Krishna <gopal_gowda@hotmail.com>
2025-05-22 21:12:20 +0530

    Commit

--- src/main/java/DaysWithoutMeetings.java
+++ src/main/java/DaysWithoutMeetings.java
@@ -1,4 +1,31 @@
 package src.main.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class DaysWithoutMeetings {
+
+    public static void main(String[] args) {
+
+        int days = 10;
+        int[][] meetings = {{5,7}, {1,3}, {9,10}};
+        Arrays.sort(meetings, Comparator.comparingInt(i -> i[0]));
+
+        System.out.println("Sorted meetings : ");
+
+        for(int[] a : meetings){
+            System.out.println(Arrays.toString(a));
+        }
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] newInterval = meetings[0];
+        result.add(newInterval);
+
+        for(int[] meeting : meetings){
+
+        }
+    }
 }
